1a2506683e0153f81156b54f60894ee503a9b003
Louis Récopé <75600561+louisdtb@users.noreply.github.com>
2022-05-17 17:47:19 +0200

    started converting pagination to CSS grid

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Box, Button, HStack } from "@chakra-ui/react";
+import styles from "./pagination.module.sass";
 
 function Pagination({ itemsPerPage, totalPages, paginate, itemSelected }) {
   const [currentPage, setCurrentPage] = React.useState(1);
@@ -17,29 +18,31 @@ function Pagination({ itemsPerPage, totalPages, paginate, itemSelected }) {
   }
 
   return (
-    <>
-      {pageNumbers.map((number) => (
-        <Box px={{ base: "4", md: "6" }} pb="5" key={number}>
-          <HStack spacing="3" justify="space-between">
-            <Box
-              spacing="3"
-              justifyContent="space-between"
-              width={{ base: "full", md: "auto" }}
-              flexDirection={"row"}
-            >
-              <Button
-                // style bg color and hover color and page selected current page
-                bg={currentPage === number ? "blue.500" : "gray.400"}
-                borderRadius="100%"
-                onClick={() => handlePageClick(number)}
+    <div className={styles.wrapper}>
+      <div className={styles.pagination}>
+        {pageNumbers.map((number) => (
+          <Box px={{ base: "4", md: "6" }} pb="5" key={number}>
+            <HStack spacing="3" justify="space-between">
+              <Box
+                spacing="3"
+                justifyContent="space-between"
+                width={{ base: "full", md: "auto" }}
+                flexDirection={"row"}
               >
-                {number}
-              </Button>
-            </Box>
-          </HStack>
-        </Box>
-      ))}
-    </>
+                <Button
+                  // style bg color and hover color and page selected current page
+                  bg={currentPage === number ? "blue.500" : "gray.400"}
+                  borderRadius="100%"
+                  onClick={() => handlePageClick(number)}
+                >
+                  {number}
+                </Button>
+              </Box>
+            </HStack>
+          </Box>
+        ))}
+      </div>
+    </div>
   );
 }
 
