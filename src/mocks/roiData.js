361c89c381ba1dc5199fe65e0442f2807e7273e9
Christian <christiank@tekimax.com>
2022-06-01 09:35:21 -0400

    mock

---
+++ src/mocks/roiData.js
@@ -0,0 +1,6 @@
+export const roiData = [
+  { companyName: "Arcane Tinmen ApS", percentageOwned: 100 },
+  { companyName: "Beckett Collectables", percentageOwned: 100 },
+  { companyName: "Comic Book Certification Service LLC", percentageOwned: 100 },
+  { companyName: "Southern Hobby Distribution, LLC", percentageOwned: 100 },
+];
