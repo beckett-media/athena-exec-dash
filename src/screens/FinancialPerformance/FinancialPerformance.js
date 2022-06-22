0059e84f22cecbd7bd8cc19fa8f7a47717836986
Nasser Qadri <nasserqadri@gmail.com>
2022-06-21 22:36:18 -0400

    nasser await changes

--- src/screens/FinancialPerformance/FinancialPerformance.js
+++ src/screens/FinancialPerformance/FinancialPerformance.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TooltipGlodal from "../../components/TooltipGlodal";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 
@@ -22,34 +22,66 @@ import balance_monthly from "../../mocks/financial_update/balance_monthly.json";
 import balance_quarterly from "../../mocks/financial_update/balance_quarterly.json";
 import balance_pivot_quarterly from "../../mocks/financial_update/balance_pivot_quarterly.json";
 
-const OpsPerformance = () => {
-  const quartly = profit_pivot_quarterly;
-  const monthly = profit_monthly;
-  const quarterly = profit_quarterly;
-
-  const revenueStreams = revenue_monthly;
-  const revenueStreamsQuarterly = revenue_quarterly;
-  const revenueStreamsPivotQuarterly = revenue_pivot_quarterly;
-
-  const balanceSheet = balance_monthly;
-  const balanceQuarterly = balance_quarterly;
-  const balancePivotQuarterly = balance_pivot_quarterly;  
+// const OpsPerformance = () => {
   
-  // const quartly = download('data_pivot_quarterly').then((result) => {return result});
-  downloadJSONs().then((results) => {
-    return opsPerformance(results);
-  });
-  // const dictionary = {'data_pivot_quarterly': quartly, 'data_monthly': monthly, 'data_quarterly': quarterly, 
-  //                        'revenue_monthly': revenueStreams, 'revenue_quarterly': revenueStreamsQuarterly, 
-  //                        'revenue_pivot_quarterly': revenueStreamsPivotQuarterly,
-  //                        'balance_monthly': balanceSheet, 'balance_quarterly': balanceQuarterly, 
-  //                        'balance_pivot_quarterly': balancePivotQuarterly}
-  // return opsPerformance(dictionary)
-};
+  
+//   // const quartly = download('data_pivot_quarterly').then((result) => {return result});
+  
+//   // const dictionary = {'data_pivot_quarterly': quartly, 'data_monthly': monthly, 'data_quarterly': quarterly, 
+//   //                        'revenue_monthly': revenueStreams, 'revenue_quarterly': revenueStreamsQuarterly, 
+//   //                        'revenue_pivot_quarterly': revenueStreamsPivotQuarterly,
+//   //                        'balance_monthly': balanceSheet, 'balance_quarterly': balanceQuarterly, 
+//   //                        'balance_pivot_quarterly': balancePivotQuarterly}
+//   // return opsPerformance(dictionary)
+// };
 
-function opsPerformance(opsDictionary) {
+
+
+
+
+function OpsPerformance() {
+
+  // const quartly = profit_pivot_quarterly;
+  // const monthly = profit_monthly;
+  // const quarterly = profit_quarterly;
+
+  // const revenueStreams = revenue_monthly;
+  // const revenueStreamsQuarterly = revenue_quarterly;
+  // const revenueStreamsPivotQuarterly = revenue_pivot_quarterly;
+
+  // const balanceSheet = balance_monthly;
+  // const balanceQuarterly = balance_quarterly;
+  // const balancePivotQuarterly = balance_pivot_quarterly;  
+
+  const [opsDictionary, setOpsDictionary] = useState({})
+  const [isLoading, setIsLoading] = useState(true)
+
+
+  const dataNames = ['data_monthly','data_quarterly','data_pivot_quarterly','revenue_monthly','revenue_quarterly','revenue_pivot_quarterly','balance_monthly','balance_quarterly','balance_pivot_quarterly']
+  // usage
+  async function downloadJSONs() {
+    const dataDict ={};
+    await Promise.all(dataNames.map(async (key) => {
+      const fileKey = 'finance-data/' + key + '.json'
+      const result = await Storage.get(fileKey, { download: true });
+      dataDict[key] = (JSON.parse(await result.Body.text()))
+    }))
+    setOpsDictionary(dataDict);
+    setIsLoading(false);
+  }
+  
+  useEffect(() => {
+    downloadJSONs()
+  }, [isLoading])
+
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  } else {
+
   return (
     <>
+    
       {/* <ComingSoon /> */}
       <Tabs
         isManual
@@ -127,32 +159,24 @@ function opsPerformance(opsDictionary) {
       </Tabs>
     </>
   );
+          }
 }
+
+
+export default OpsPerformance;
 
 function isLoading() {
   return "Loading..."
 }
 
-const fileNames = ['finance-data/balance_monthly.json', 'finance-data/balance_pivot_quarterly.json', 'finance-data/balance_quarterly.json', 'finance-data/data_monthly.json', 'finance-data/data_pivot_quarterly.json', 'finance-data/data_quarterly.json', 'finance-data/revenue_monthly.json', 'finance-data/revenue_pivot_quarterly.json', 'finance-data/revenue_quarterly.json']
+// const fileNames = ['finance-data/balance_monthly.json', 'finance-data/balance_pivot_quarterly.json', 'finance-data/balance_quarterly.json', 'finance-data/data_monthly.json', 'finance-data/data_pivot_quarterly.json', 'finance-data/data_quarterly.json', 'finance-data/revenue_monthly.json', 'finance-data/revenue_pivot_quarterly.json', 'finance-data/revenue_quarterly.json']
 
-function validateStorage() {
-  const storageList = Storage.list('').then((result) => {
-    const storedFolders = result.map((x) => (x['key']));
-    return fileNames.every(fileName => {return storedFolders.includes(fileName)});
-  });
-  return storageList;
-}
+// function validateStorage() {
+//   const storageList = Storage.list('').then((result) => {
+//     const storedFolders = result.map((x) => (x['key']));
+//     return fileNames.every(fileName => {return storedFolders.includes(fileName)});
+//   });
+//   return storageList;
+// }
 
-export default OpsPerformance;
 
-const dataNames = ['data_monthly','data_quarterly','data_pivot_quarterly','revenue_monthly','revenue_quarterly','revenue_pivot_quarterly','balance_monthly','balance_quarterly','balance_pivot_quarterly']
-// usage
-async function downloadJSONs() {
-  const dataDict = {};
-  await Promise.all(dataNames.map(async (key) => {
-    const fileKey = 'finance-data/' + key + '.json'
-    const result = await Storage.get(fileKey, { download: true });
-    dataDict[key] = (JSON.parse(await result.Body.text()));
-  }))
-  return dataDict;
-}
